68157afb0a47a8b73ce137685f119f1d45b3d803
Zehavit <levi.zehavit@gmail.com>
2021-04-29 09:50:31 +0300

    fix empty string issue

--- src/components/NewTodoInput/NewTodoInput.js
+++ src/components/NewTodoInput/NewTodoInput.js
@@ -6,17 +6,19 @@ export default function NewTodoInput(props) {
     const [newTodo, setNewTodo] = useState("");
     const handleSubmit = (e, props) => {
         e.preventDefault();
-        props.addTodoProps(newTodo);
+        if(newTodo !== "") props.addTodoProps(newTodo);
         setNewTodo("");
     }
 
-    const handleChangeNewTodo = (e) => {
-        e.preventDefault();
-        setNewTodo(e.target.value);
-    }
-    return (
-        <Form className="todo-form" onSubmit={(e) => handleSubmit(e, props)}>
-            <Form.Control type="text" placeholder="What's next?" value={newTodo} onChange={(e) => handleChangeNewTodo(e)} />
-        </Form>
-    )
+
+const handleChangeNewTodo = (e) => {
+    e.preventDefault();
+    setNewTodo(e.target.value)
+};
+
+return (
+    <Form className="todo-form" onSubmit={(e) => handleSubmit(e, props)}>
+        <Form.Control type="text" placeholder="What's next?" value={newTodo} onChange={(e) => handleChangeNewTodo(e)} />
+    </Form>
+)
 }
